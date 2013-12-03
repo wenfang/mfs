6180dc5c96fe913fc186bb9d7481f1ad249a1fa6
wenfang <wangwenfang@gmail.com>
2013-12-03 18:36:14 +0800

    add

--- src/mfs/meta.go
+++ src/mfs/meta.go
@@ -1,5 +1,39 @@
 package mfs
 
+import (
+  "errors"
+  "os"
+)
+
+const (
+	ImgOK = iota
+	ImgFail
+	ImgStop
+)
+
+const (
+  MetaSize = 
+	MaxImg = 64
+)
+
+type imgFile struct {
+	ImgName string
+	ImgStat uint16
+}
+
 type Meta struct {
-  SiteId uint32
+	SiteId  uint32
+	ImgNum  uint16
+	ImgFile [MaxImg]imgFile
+}
+
+func NewMeta(metaFile string) (*Meta, error) {
+  f, err := os.Open(metaFile)
+  if err != nil {
+    return nil, errors.New("Meta Open File Error")
+  }
+  defer f.Close()
+
+	meta := new(Meta)
+	return meta, nil
 }
